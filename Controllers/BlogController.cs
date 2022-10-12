db343bdeca89b08ddaa0ed9caf5cb685f0fc2b65
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2022-10-12 10:32:55 +0700

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRAVEL.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Blog_post()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ToursController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRAVEL.Controllers
+{
+    public class ToursController : Controller
+    {
+        // GET: Tours
+        public ActionResult Tours_grid()
+        {
+            return View();
+        }
+    }
+}
