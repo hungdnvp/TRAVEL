aaed30876e18c1deae6f0faa7e8087cbc17b6f15
kptis <shinkhong97@gmail.com>
2022-10-14 08:52:44 +0700

    fix db connection code-first+capchactrl

---
+++ Controllers/CaptchaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRAVEL.Controllers
+{
+     [AllowAnonymous]
+    public class CaptchaController : Controller
+    {
+        // GET: Captcha
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FromSubmit()
+          {
+
+          }
+
+    }
+
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TRAVEL.Models;
 
 namespace TRAVEL.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+     public class HomeController : Controller
+     {
+          private TravelVietNamEntities ObjTravelModel = new TravelVietNamEntities();
+          public ActionResult Index()
+          {
+               return View();
+          }
 
-            return View();
-        }
+          public ActionResult Login()
+          {
+               //ViewBag.Message = "Your application description page.";
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+               return View();
+          }
 
-            return View();
-        }
-    }
+          public ActionResult About()
+          {
+               ViewBag.Message = "Your application description page.";
+
+               return View();
+          }
+
+          public ActionResult Contact()
+          {
+               ViewBag.Message = "Your contact page.";
+
+               return View();
+          }
+     }
 }
