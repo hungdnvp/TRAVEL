f6454a43ee3aae1d183d00649ccb5a125aa290c4
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2022-10-22 13:56:12 +0700

    select,show tours

--- Controllers/ToursController.cs
+++ Controllers/ToursController.cs
@@ -4,26 +4,110 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TRAVEL.Models;
+using Newtonsoft;
 
 namespace TRAVEL.Controllers
 {
     public class ToursController : Controller
     {
         // GET: Tours
+        Dictionary<int, Nullable<int>> prices = new Dictionary<int, Nullable<int>>();
+        Dictionary<int, Nullable<int>> numstars = new Dictionary<int, Nullable<int>>();
         public ActionResult Tours_grid()
         {
             using (MyDbContext Travel = new MyDbContext())
             {
                 var tours = Travel.Tours.ToList();
+                foreach (Tour it in tours)
+                {
+                    Nullable<int> price = 999;
+                    float star = 0f;
+                    try
+                    {
+                        var bg = Travel.BangGias.FirstOrDefault(s => s.MaTour == it.MaTour);
+                        if (bg != null)
+                        {
+                            price = bg.GiaNQ_NguoiLon;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("error get price");
+                    }
+                    this.prices.Add(it.MaTour, price);
+
+                    try
+                    {
+                        var dgs = Travel.DanhGias.Where(s => s.MaTour == it.MaTour).ToList();
+                        foreach (var dg in dgs)
+                        {
+                            if (dg.NumStar != null) star += (float)dg.NumStar;
+                        }
+                        star /= dgs.Count();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("error get numstar");
+                    }
+                    this.numstars.Add(it.MaTour, (int)Math.Round(star));
+                }
+
+                ViewBag.Prices = prices;
+                ViewBag.Numstars = numstars;
                 return View(tours);
 
             }
 
         }
 
-        public ActionResult AjaxView(IEnumerable<Tour> tourlist)
+        [HttpPost]
+        public ActionResult AjaxView(int type)
         {
-            return PartialView("_tourlist");
+            using (MyDbContext Travel = new MyDbContext())
+            {
+                var tours = Travel.Tours.ToList();
+                foreach (Tour it in tours)
+                {
+                    Nullable<int> price = 999;
+                    float star = 0f;
+                    try
+                    {
+                        var bg = Travel.BangGias.FirstOrDefault(s => s.MaTour == it.MaTour);
+                        if (bg != null)
+                        {
+                            price = bg.GiaNQ_NguoiLon;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("error get price");
+                    }
+                    this.prices.Add(it.MaTour, price);
+
+                    try
+                    {
+                        var dgs = Travel.DanhGias.Where(s => s.MaTour == it.MaTour).ToList();
+                        foreach (var dg in dgs)
+                        {
+                            if (dg.NumStar != null) star += (float)dg.NumStar;
+                        }
+                        star /= dgs.Count();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("error get numstar");
+                    }
+                    this.numstars.Add(it.MaTour, (int)Math.Round(star));
+                }
+
+                ViewBag.Prices = prices;
+                ViewBag.Numstars = numstars;
+                if (type == 2)
+                {
+                    return PartialView("_tourlist", tours);
+                }
+                else { return PartialView("_tourgrid", tours); }
+            }
         }
     }
 }
