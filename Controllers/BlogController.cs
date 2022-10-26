f6ccedf4a473eb8aef7d35210c7fb480f4aecc6d
kptis <shinkhong97@gmail.com>
2022-10-26 11:23:56 +0700

    add GUI for sorting blog

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TRAVEL.Models;
+using PagedList;
 
 namespace TRAVEL.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
-        public ActionResult Blog_post()
+        public ActionResult Blog_post(int ? page)
         {
             MyDbContext md = new MyDbContext();
-               var model = md.Blogs.ToList();
-            return View(model);
+            var model = md.Blogs.ToList();
+               int pageSize = 6;
+               int no_of_page = (page ?? 1);
+               return View(model.ToPagedList(no_of_page, pageSize));
         }
     }
 }
