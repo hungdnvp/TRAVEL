407c1d8a94dc12c61ea35d9f55ef87ce4109d88e
kptis <shinkhong97@gmail.com>
2022-10-28 12:10:51 +0700

    Fix height div in Blog_grid + js select box for sorting

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -11,10 +11,78 @@ namespace TRAVEL.Controllers
     public class BlogController : Controller
     {
         // GET: Blog
-        public ActionResult Index(int ? page)
+        public ActionResult Index(int ? page, string sortby="",string sort_type="",string name="", string author="", string category = "", string tag="" )
         {
             MyDbContext md = new MyDbContext();
             var model = md.Blogs.ToList();
+
+               switch (sortby)
+               {
+                    case "name":
+                         if (sort_type == "ascend")
+                         {
+                              model = model.OrderBy(x => x.Ten).ToList();
+                         }
+                         else if (sort_type == "descend")
+                         {
+                              //model = model.OrderByDescending()
+                         }
+                         else
+                         {
+
+                         }
+                         break;
+                    case "post_date":
+                         if (sort_type == "ascend")
+                         {
+
+                         }
+                         else if (sort_type == "descend")
+                         {
+
+                         }
+                         else
+                         {
+
+                         }
+                         break;
+
+                    case "comments":
+
+                         if (sort_type == "ascend")
+                         {
+
+                         }
+                         else if (sort_type == "descend")
+                         {
+
+                         }
+                         else
+                         {
+
+                         }
+                         break;
+
+                    case "reactions":
+
+                         if (sort_type == "ascend")
+                         {
+
+                         }
+                         else if (sort_type == "descend")
+                         {
+
+                         }
+                         else
+                         {
+
+                         }
+                         break;
+
+                    default:
+                         model = searchModelByName(searchModelByAuthor(searchModelByCategory(searchModelByTag(model, tag), category), author), name);
+                         break;
+               }
                int pageSize = 6;
                int no_of_page = (page ?? 1);
                return View(model.ToPagedList(no_of_page, pageSize));
@@ -34,5 +102,54 @@ namespace TRAVEL.Controllers
                var Blog_dt_model = md.Blogs.Single(b => b.Blog_ID== id);
                return View(Blog_dt_model);
           }
+          public List<Blog> searchModelByName(List<Blog> model, string name)
+          {
+               if (name != null)
+               {
+                    model = model.Where(x => (x.Ten.ToUpper().Contains(name.ToUpper()))).ToList();
+               }
+               else
+               {
+                    model = model.ToList();
+               }
+               return model;
+          }
+          public List<Blog> searchModelByAuthor(List<Blog> model, string author)
+          {
+               if (author != null)
+               {
+                    model = model.Where(x => (x.Ten.ToUpper().Contains(author.ToUpper()))).ToList();
+               }
+               else
+               {
+                    model = model.ToList();
+               }
+               return model;
+          }
+          public List<Blog> searchModelByTag(List<Blog> model, string tag)
+          {
+               if (tag != null)
+               {
+                    model = model.Where(x => (x.Ten.ToUpper().Contains(tag.ToUpper()))).ToList();
+               }
+               else
+               {
+                    model = model.ToList();
+               }
+               return model;
+          }
+          public List<Blog> searchModelByCategory(List<Blog> model, string category)
+          {
+               if (category != null)
+               {
+                    model = model.Where(x => (x.Ten.ToUpper().Contains(category.ToUpper()))).ToList();
+               }
+               else
+               {
+                    model = model.ToList();
+               }
+               return model;
+          }
+
      }
 }
