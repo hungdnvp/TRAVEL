f2a53385cdd2b20a623c9174fc8f7d2cd9c28dcd
ATT21072001 <anthanhtulkt@gmail.com>
2022-10-30 22:02:12 +0700

    tu

---
+++ Areas/Admin/Controllers/ListUsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRAVEL.Areas.Admin.Controllers
+{
+    public class ListUsersController : Controller
+    {
+        // GET: Admin/ListUsers
+        public ActionResult ListUsers()
+        {
+            return View();
+        }
+    }
+}
