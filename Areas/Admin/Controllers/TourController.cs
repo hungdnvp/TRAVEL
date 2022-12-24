62f56304c537c5c85cc585416ebc7a1d806290dc
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2022-12-24 19:37:50 +0700

    complete List Tour Admin, Edit Tour

---
+++ Areas/Admin/Controllers/TourController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TRAVEL.Models;
+
+namespace TRAVEL.Areas.Admin.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Admin/Tour
+        public ActionResult List()
+        {
+            using (MyDbContext Travel = new MyDbContext())
+            {
+                List<Tour> list = new List<Tour>();
+                list = Travel.Tours.ToList();
+                return View(list);
+            }
+        }
+
+        public ActionResult Delete(int idTour)
+        {
+            using (MyDbContext Travel = new MyDbContext())
+            {
+                Tour t = Travel.Tours.Find(idTour);
+                if (t != null)
+                {
+                    t.DichVus.Clear();
+                    t.DanhGias.Clear();
+                    t.PhieuDatTours.Clear();
+                    t.BangGias.Clear();
+                    t.DiaDanhs.Clear();
+                    t.TravelTypes.Clear();
+                    Travel.Tours.Remove(t);
+                    Travel.SaveChanges();
+                }
+                return RedirectToAction("List");
+            }
+        }
+
+        public ActionResult Edit(int idTour)
+        {
+            using (MyDbContext Travel = new MyDbContext())
+            {
+
+                Tour t = Travel.Tours.Find(idTour);
+                return View(t);
+            }
+        }
+    }
+}
