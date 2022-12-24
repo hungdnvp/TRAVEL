cea574741c40f4e400a821dc8eef18aaf1780cfe
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2022-12-24 13:49:47 +0700

    complete profile user

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -32,5 +32,39 @@ namespace TRAVEL.Controllers
             }
             
         }
+
+        [HttpPost]
+        public ActionResult Upload(string email,string phoneNumber,string address,string fullName,HttpPostedFileBase image)
+        {
+            using (MyDbContext db = new MyDbContext())
+            {
+                Image img = new Image();
+                TaiKhoan account = new TaiKhoan();
+                account = (TaiKhoan)Session["online"];
+                ChiTietTK account_cttk = new ChiTietTK();
+                account_cttk = db.ChiTietTKs.Where(c => c.MaTaiKhoan == account.MaTaiKhoan).FirstOrDefault();
+                if (image !=null && image.ContentLength > 0)
+                {
+                    img.image = new byte[image.ContentLength];
+                    image.InputStream.Read(img.image, 0, image.ContentLength);
+                    string fileName = System.IO.Path.GetFileName(image.FileName);
+                    string urlImage = Server.MapPath("~/assets/img/avatar/" + fileName);
+                    image.SaveAs(urlImage);
+
+                    account_cttk.LinkAvatar = "img/avatar/" + fileName;
+                }
+                if (!String.IsNullOrEmpty(email))
+                {
+                    account.email = email;
+                }
+                if (!String.IsNullOrEmpty(phoneNumber)) account_cttk.SDT = phoneNumber;
+                if (!String.IsNullOrEmpty(address)) account_cttk.DiaChi = address;
+
+                db.SaveChanges();
+            }
+
+            
+            return RedirectToAction("Profile");
+        }
     }
 }
